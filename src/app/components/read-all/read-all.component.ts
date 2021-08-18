ad8cfc5309e8d3ad8ba2f2c59e80e1b3fb01499d
Vitor <vitorsimoesdarosa2001@gmail.com>
2021-08-18 12:53:01 -0300

    ToDo

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -36,7 +36,7 @@ export class ReadAllComponent implements OnInit {
   finalizar(item: Todo): void {
     item.finalizado = true;
     this.service.update(item).subscribe(() => {
-      this.service.message("Task finalizada com sucesso!");
+      this.service.message("Tarefas finalizada com sucesso!");
       this.list = this.list.filter((todo) => todo.id !== item.id);
       this.closed++;
     });
@@ -45,7 +45,7 @@ export class ReadAllComponent implements OnInit {
   delete(id: any): void {
     this.service.delete(id).subscribe((resposta) => {
       if (resposta === null) {
-        this.service.message("Task deletada com sucesso!");
+        this.service.message("Tarefas deletada com sucesso!");
         this.list = this.list.filter((todo) => todo.id !== id);
       }
     });
